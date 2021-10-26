9cc08a0d146b27af6c82744c6511e4bacf4f782b
Mauro Goyeneche <48460577+maurogoyeneche@users.noreply.github.com>
2021-10-26 15:29:46 -0300

    day_04 completed

--- src/day_04/arrays.test.js
+++ src/day_04/arrays.test.js
@@ -28,50 +28,56 @@ describe('DAY 4: Test Arrays', () => {
 
     it('Array to be instance of Array (literal)', () => {
         // use literal notation
-        let array;
+        let array = [];
         expect(array).toBeInstanceOf(Array);
     });
 
     it('Array to be instance of Array (constructor)', () => {
         // use constructor
-        let array;
+        let array = new Array([]);
         expect(array).toBeInstanceOf(Array);
     });
 
     it('Array to be instance of Object and Array', () => {
         // use any of the previous
-        let array;
+        let array = [];
         expect(array).toBeInstanceOf(Array);
         expect(array).toBeInstanceOf(Object);
     });
 
     it('Initialize Array with length of 4 (literal)', () => {
         // use literal
-        let array;
+        let array = [4, 3, 2, 1];
         expect(array.length).toBe(4);
     });
 
     it('Initialize Array with length of 4 (constructor)', () => {
         // use constructor
-        let array;
+        let array = new Array(4, 3, 5, 6);
+
         expect(array.length).toBe(4);
     });
 
     it('Initialize Array with length of 1 (constructor), and first entry value to be 4', () => {
         // use constructor
-        let array;
+        let array = new Array(1);
+        array[0] = 4;
+        // falla
+
         expect(array.length).toBe(1);
         expect(array[0]).toBe(4);
     });
 
     it('Mutate Array length to be 4', () => {
         let array = [];
+        array.push(1, 2, 3, 4);
         // use a mutator method to add elements
         expect(array.length).toBe(4);
     });
 
     it('Mutate Array length to be 5', () => {
         let array = [42, 42, 42, 42, 42, 42];
+        array.pop();
         // use a mutator method to remove elements
         expect(array.length).toBe(5);
     });
@@ -79,26 +85,30 @@ describe('DAY 4: Test Arrays', () => {
     it('Remove the first element of the array', () => {
         let array = ['a', 'b', 'c', 'd'];
         // use a method to remove it
+        array.shift();
         expect(array.indexOf('a')).toBe(-1);
     });
 
     it('Remove the last element of the array', () => {
         let array = ['a', 'b', 'c', 'd'];
         // use a method to remove it
+        array.pop();
         expect(array.indexOf('d')).toBe(-1);
     });
 
     it('Remove an element off from inner boundaries of the array', () => {
         let array = ['a', 'b', 'c', 'd'];
         // use a method to remove it
+        array.splice(1, 1);
         expect(array.indexOf('b')).toBe(-1);
     });
 
     it('Create a new array containing the values of 2 different arrays (method)', () => {
         let array1 = ['a', 'b', 'c', 'd'];
         let array2 = [1, 2, 3, 4, 5];
+
+        let array3 = array2.concat(array1);
         // use a method to create it
-        let array3;
 
         expect(array3).toEqual([1, 2, 3, 4, 5, 'a', 'b', 'c', 'd']);
     });
@@ -107,7 +117,7 @@ describe('DAY 4: Test Arrays', () => {
         let array1 = ['a', 'b', 'c', 'd'];
         let array2 = [1, 2, 3, 4, 5];
         // use an operator to create it
-        let array3;
+        let array3 = [...array1, ...array2];
 
         expect(array3).toEqual(['a', 'b', 'c', 'd', 1, 2, 3, 4, 5]);
     });
@@ -115,7 +125,7 @@ describe('DAY 4: Test Arrays', () => {
     it('Create a string from an array (method)', () => {
         let array = ['a', 'b', 'c', 'd'];
         // use a method to create it
-        let string;
+        let string = array.join('-');
 
         expect(string).toBe('a-b-c-d');
     });
@@ -123,7 +133,7 @@ describe('DAY 4: Test Arrays', () => {
     it('Create an array from a string (method)', () => {
         let string = 'a-b-c-d';
         // use a method to create it
-        let array;
+        let array = string.split('-');
 
         expect(array).toEqual(['a', 'b', 'c', 'd']);
     });
@@ -131,7 +141,8 @@ describe('DAY 4: Test Arrays', () => {
     it('Create an array from a string (operator)', () => {
         let string = 'abcd';
         // use an operator to create it
-        let array;
+        let array = string.split('');
+        // falla
 
         expect(array).toEqual(['a', 'b', 'c', 'd']);
     });
@@ -139,7 +150,7 @@ describe('DAY 4: Test Arrays', () => {
     it('Reverse the a string using the learned techniques', () => {
         let string = 'abcd';
         // use available method for strings an arrays
-        let reversed;
+        let reversed = string.split('').reverse().join('');
 
         expect(reversed).toEqual('dcba');
     });
@@ -147,6 +158,7 @@ describe('DAY 4: Test Arrays', () => {
     it('Copy the values of array a to array b using an iteration', () => {
         let a = [1, 2, 'a', 'b', 'c', [6, 7]];
         let b = [];
+        for (let i in a) { b.push(a[ i ]); }
 
         /**
          * 
@@ -170,7 +182,7 @@ describe('DAY 4: Test Arrays', () => {
         let array = hotPeppers;
         let myFavorite = 'Habanero';
         // use an array method
-        let found;
+        let found = array.find(el => el === myFavorite) ;
 
         expect(found).toBe(myFavorite);
     });
